0535b887cb791bdf2a55e1e15216b69e023b1fe3
Izadora Braga <izadora_abraga@outlook.com>
2023-11-14 16:49:02 -0300

    requisito 3 iniciando

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('Favorite Pokémon', () => {
+  test('mensagem caso não tenha favorito', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ [] } />);
+
+    expect(screen.getByText('No favorite Pokémon found')).toBeInTheDocument();
+  });
+
+  test('Apenas Pokémon favoritados', () => {
+  });
+});
+
+// npx stryker run ./stryker/FavoritePokemon.conf.json
+// test('', () => {});
