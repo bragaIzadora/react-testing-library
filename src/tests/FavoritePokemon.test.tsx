7995e6849f35cfe56a5a4c0e5bb553eb4976f3b8
Izadora Braga <izadora_abraga@outlook.com>
2023-11-14 19:21:54 -0300

    requisito 3 finalizado

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,6 +1,7 @@
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemon } from '../pages';
+import App from '../App';
 
 describe('Favorite Pokémon', () => {
   test('mensagem caso não tenha favorito', () => {
@@ -9,7 +10,20 @@ describe('Favorite Pokémon', () => {
     expect(screen.getByText('No favorite Pokémon found')).toBeInTheDocument();
   });
 
-  test('Apenas Pokémon favoritados', () => {
+  test('Apenas Pokémon favoritados', async () => {
+    const { user } = renderWithRouter(<App />);
+
+    const moreDetailsLink = screen.getByRole('link', { name: /more details/i });
+    await user.click(moreDetailsLink);
+
+    const checkbox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    await user.click(checkbox);
+
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémon/i });
+    await user.click(favoriteLink);
+
+    const pokemonNameElement = screen.getByTestId('pokemon-name');
+    expect(pokemonNameElement.textContent).toContain('Pikachu');
   });
 });
 
